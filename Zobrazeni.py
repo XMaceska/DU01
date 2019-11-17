7fb53e0f78e193cca82859cec8ed94bf573343b0
David Maceska <dave.maceska@seznam.cz>
2019-11-17 23:12:13 +0100

    První tvorba kostry programu

--- Zobrazeni.py
+++ Zobrazeni.py
@@ -8,10 +8,19 @@ R = 6371.11
 
 a = input("Na výběr z několika zobrazení: \n\nL pro Lambertovo zobrazení \nA pro Marinovo zobrazení \nB pro Braunovo zobrazení \nM pro Mercatorovo zobrazení\n\nZvolte požadované zobrazení:")
 
-sirka = int(input("vlozte zeměpisnou šířku:"))
-#delka = int(input("vlozte zeměpisnou délku:"))
+def Lambert(R):
+    print("lol")
+def Marin(R):
+    print("rofl")
+def Braun(R):
+    print("ok")
+def Mercator(R):
+    print("not ok")
 
 while a == "L":
-    print("vzdálenost je:", 2*R*sin(sirka/2))
-if a == "A":
-    print("x je:", R*delka,"\n","y je", R*sirka)
+    print("Lambertovo zobrazení:")
+    Lambert
+
+
+
+
