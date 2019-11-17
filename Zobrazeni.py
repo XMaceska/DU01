0ecd07ee4ff7e9ef8b7d2e7ce710116e6725e46a
David Maceska <dave.maceska@seznam.cz>
2019-11-17 22:31:30 +0100

    Input zobrazení, první pokus o výpočet vzorců

--- Zobrazeni.py
+++ Zobrazeni.py
@@ -1,2 +1,17 @@
-print ("hello")
-print (2+3)
+from math import sin, tan
+import sys
+
+
+# definice proměnných
+
+R = 6371.11
+
+a = input("Na výběr z několika zobrazení: \n\nL pro Lambertovo zobrazení \nA pro Marinovo zobrazení \nB pro Braunovo zobrazení \nM pro Mercatorovo zobrazení\n\nZvolte požadované zobrazení:")
+
+sirka = int(input("vlozte zeměpisnou šířku:"))
+#delka = int(input("vlozte zeměpisnou délku:"))
+
+while a == "L":
+    print("vzdálenost je:", 2*R*sin(sirka/2))
+if a == "A":
+    print("x je:", R*delka,"\n","y je", R*sirka)
