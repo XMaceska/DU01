3422441e9a7ab1689d03b2d0e8fa7aa356aa27dd
David Maceska <dave.maceska@seznam.cz>
2019-11-18 20:27:52 +0100

    Definice první funkce zobrzaení (Lambertovo)

--- Zobrazeni.py
+++ Zobrazeni.py
@@ -4,15 +4,24 @@ import sys
 
 # definice proměnných
 
-R = 6371.11
+R = 6371.11         # poloměr země [km]
+c = 10              # vzdálenost mezi souřadnicemi [°]
 
-a = input("Na výběr z několika zobrazení: \n\nL pro Lambertovo zobrazení \nA pro Marinovo zobrazení \nB pro Braunovo zobrazení \nM pro Mercatorovo zobrazení\n\nZvolte požadované zobrazení:")
+Rovnobezky = []
+Poledniky = []
 
+a = input("Na výběr z několika válcových tečných zobrazení: \n\nL pro Lambertovo zobrazení \nA pro Marinovo zobrazení \nB pro Braunovo zobrazení \nM pro Mercatorovo zobrazení\n\nZvolte požadované zobrazení:")
 b = int(input("Zvolte požadované měřítko:",))
 
 def lambert(R):
-    for v in range (-180,180):
-        x = 10
+
+    for v in range(-180,180,c):      # funkce range (start, stop, step)
+        x = R*v
+        Rovnobezky.append(x)
+    for u in range(-90, 90, c):
+        y = R*u
+        Poledniky.append(y)
+
 
 
 def marin(R):
@@ -23,9 +32,15 @@ def mercator(R):
     print("not ok")
 
 if a == "L":
-    print("Lambertovo zobrazení:", lambert(R))
+    lambert(R)
+    print("Lambertovo zobrazení:\n""Rovnoběžky:", Rovnobezky,"\nPoledníky:", Poledniky)
+
 elif a == "A":
-    print(marin(R))
+    print("Marinovo zobrazení:\n""Rovnoběžky:", Rovnobezky,"\n","Poledníky:", Poledniky)
+
+else:
+    print("pro písmeno",a, "není definované žádné zobrazení")
+    exit()
 
 
 
