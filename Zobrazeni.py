b92aed301a6f3f42d81e2792d0a1ab0e3db0decd
David Maceska <dave.maceska@seznam.cz>
2019-11-18 23:04:51 +0100

    Přidána funkce na volitelný poloměr země

--- Zobrazeni.py
+++ Zobrazeni.py
@@ -1,6 +1,4 @@
-from math import sin, tan, log1p, 
-import sys
-
+from math import sin, tan, log, radians
 
 # definice proměnných
 
@@ -11,7 +9,25 @@ rovnobezky = []
 poledniky = []
 
 a = input("Na výběr z několika válcových tečných zobrazení: \n\nL pro Lambertovo zobrazení \nA pro Marinovo zobrazení \nB pro Braunovo zobrazení \nM pro Mercatorovo zobrazení\n\nZvolte požadované zobrazení:")
-b = int(input("Zvolte požadované měřítko:",))
+b = int(input("Zvolte požadované měřítko: ",))
+
+polomer = input("Chcete zvolit vlastní poloměr země? A/N: ")
+while polomer == "A":
+    R = int(input("Zvolte požadovaný poloměr země v celých číslech prosím: ",))
+    while len(str((R))) == 4:
+        print("zadal(a) jste poloměr země",R, "v jednotkách km")
+        R = R*100000
+    if len(str((R))) == 7:
+        print("zadal(a) jste poloměr země",R, "v jednotkách m")
+        R = R*100
+    if len(str((R))) == 9:
+        print("zadal(a) jste poloměr země", R, "v jednotkách cm")
+        R = R
+        break
+if polomer == "N":
+    R = R*100000
+
+
 
 
 # tvorba funkcí ze vzorců pro zobrazení
@@ -20,10 +36,13 @@ b = int(input("Zvolte požadované měřítko:",))
 def lambert(R):
     for v in range(-180, 180, c):      # funkce range (start, stop, step)
         x = R*v
-        poledniky.append(x)
+        x_vypocet_meritka = round(x/b,1)
+        poledniky.append(x_vypocet_meritka)
+
     for u in range(-90, 90, c):
-        y = R*sin(u)
-        rovnobezky.append(y)
+        y = R*sin(radians(u))
+        y_vypocet_meritka = round(y/b,1)
+        rovnobezky.append(y_vypocet_meritka)
 
 def marin(R):
     for v in range(-180, 180, c):
@@ -38,22 +57,32 @@ def braun(R):
         x = R*v
         poledniky.append(x)
     for u in range(-90, 90, c):
-        y = 2*R*tan(u/2)
+        y = 2*R*tan(radians(u)/2)
         rovnobezky.append(y)
 
 def mercator(R):
     for v in range(-180, 180, c):
         x = R*v
         poledniky.append(x)
-    for u in range(-90, 90, c):
-        y = R*log1p()
+    for u in range(-90, 91, c):
+        y = R*log(1/tan(radians(u)/2))
+        rovnobezky.append(y)
 
 if a == "L":
     lambert(R)
     print("Lambertovo zobrazení:\n""Rovnoběžky:", rovnobezky, "\nPoledníky:", poledniky)
 
 elif a == "A":
+    marin(R)
     print("Marinovo zobrazení:\n""Rovnoběžky:", rovnobezky, "\n", "Poledníky:", poledniky)
+
+elif a == "B":
+    braun(R)
+    print("Braunovo zobrazení:\n""Rovnoběžky:", rovnobezky, "\n", "Poledníky:", poledniky)
+
+elif a == "M":
+    mercator(R)
+    print("Mercatorovo zobrazení:\n""Rovnoběžky:", rovnobezky, "\n", "Poledníky:", poledniky)
 
 else:
     print("pro písmeno",a, "není definované žádné zobrazení")
