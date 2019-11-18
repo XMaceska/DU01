6a1e5692061c2f8a2f2a8f8600f9736972a24419
David Maceska <dave.maceska@seznam.cz>
2019-11-18 20:47:47 +0100

    Definice všech funkcí zobrzaení

--- Zobrazeni.py
+++ Zobrazeni.py
@@ -1,4 +1,4 @@
-from math import sin, tan
+from math import sin, tan, log1p, 
 import sys
 
 
@@ -7,36 +7,53 @@ import sys
 R = 6371.11         # poloměr země [km]
 c = 10              # vzdálenost mezi souřadnicemi [°]
 
-Rovnobezky = []
-Poledniky = []
+rovnobezky = []
+poledniky = []
 
 a = input("Na výběr z několika válcových tečných zobrazení: \n\nL pro Lambertovo zobrazení \nA pro Marinovo zobrazení \nB pro Braunovo zobrazení \nM pro Mercatorovo zobrazení\n\nZvolte požadované zobrazení:")
 b = int(input("Zvolte požadované měřítko:",))
 
+
+# tvorba funkcí ze vzorců pro zobrazení
+# v = poledniky, u = rovnobezky
+
 def lambert(R):
-
-    for v in range(-180,180,c):      # funkce range (start, stop, step)
+    for v in range(-180, 180, c):      # funkce range (start, stop, step)
         x = R*v
-        Rovnobezky.append(x)
+        poledniky.append(x)
     for u in range(-90, 90, c):
-        y = R*u
-        Poledniky.append(y)
-
-
+        y = R*sin(u)
+        rovnobezky.append(y)
 
 def marin(R):
-    return "rofl"
+    for v in range(-180, 180, c):
+        x = R*v
+        poledniky.append(x)
+    for u in range(-90, 90, c):
+        y = R*u
+        rovnobezky.append(y)
+
 def braun(R):
-    print("ok")
+    for v in range(-180, 180, c):
+        x = R*v
+        poledniky.append(x)
+    for u in range(-90, 90, c):
+        y = 2*R*tan(u/2)
+        rovnobezky.append(y)
+
 def mercator(R):
-    print("not ok")
+    for v in range(-180, 180, c):
+        x = R*v
+        poledniky.append(x)
+    for u in range(-90, 90, c):
+        y = R*log1p()
 
 if a == "L":
     lambert(R)
-    print("Lambertovo zobrazení:\n""Rovnoběžky:", Rovnobezky,"\nPoledníky:", Poledniky)
+    print("Lambertovo zobrazení:\n""Rovnoběžky:", rovnobezky, "\nPoledníky:", poledniky)
 
 elif a == "A":
-    print("Marinovo zobrazení:\n""Rovnoběžky:", Rovnobezky,"\n","Poledníky:", Poledniky)
+    print("Marinovo zobrazení:\n""Rovnoběžky:", rovnobezky, "\n", "Poledníky:", poledniky)
 
 else:
     print("pro písmeno",a, "není definované žádné zobrazení")
